d3ca4ec5af48c4acc89a302a0abccc1070a7de12
profLedson <ledson.vanini@escola.pr.gov.br>
2026-02-23 12:36:42 +0000

    Criar variaveis para Pessoa e Produto

---
+++ variaveis/variavel-pessoa.js
@@ -0,0 +1,16 @@
+// , var, let, const (valores reservados da linguagem)
+// 0-9, 0,7, 0-1, 0-9,A,B,C,D,E,F
+
+let nome = 'Petter'         // string
+let sobrenome = 'Parker'    // string
+const cpf = "888888888-88"  // string
+const rg = '101212 ssp'     // string
+let idade = 22              // number
+let soleitro = true         // boolean
+let nota = 7 + 2            // number
+
+console.log(nome)
+// imprima aqui todas as variáveis acima
+
+
+console.log(typeof nome)
---
+++ variaveis/variavel-produto.js
@@ -0,0 +1,13 @@
+/**
+ * CRIE 5 VARIÁVEIS PARA CARACTERÍSTICAS DE UM PRODUTO DO MERCADO LIVRE
+ * CONSIDERE SE A VARIÁVEL ,É let ou const
+ * IMPRIMA COM console.log(<varivel>); 
+ * IMPRIMA COM console.log("NOME-VARIAVEL: " + <variavel>)
+ */
+let nomeProduto = "camisa"
+
+
+
+
+console.log(nomeProduto)
+console.log("Nome: " + nomeProduto)
