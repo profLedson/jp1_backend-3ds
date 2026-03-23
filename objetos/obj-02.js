58210413dbe3ae1b249b15bae4520132197b1329
profLedson <ledson.vanini@escola.pr.gov.br>
2026-03-23 12:05:53 +0000

    Finalização de Objetos js

---
+++ objetos/obj-02.js
@@ -0,0 +1,9 @@
+// Crie um Objeto do tema que escolher
+// Crie pelo menos 5 propriedades
+// Faça uma impressão para Humanos!
+// ex. console.log("Nome do Jogador: " + jogador.nome)
+
+const personagems = [
+    {nome: "x", },
+    {nome: "x", }
+]
