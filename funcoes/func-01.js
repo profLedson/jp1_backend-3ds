cae669bc1a5d4a4461de41a91c88ea8f81adf5e7
profLedson <ledson.vanini@escola.pr.gov.br>
2026-03-30 10:56:46 +0000

    Funções em Javascript

---
+++ funcoes/func-01.js
@@ -0,0 +1,16 @@
+// Funções são blocos de código reaproveitáveis
+// podem ou Não receber parâmetros
+// podem ou não retornar valores
+
+// declaração
+function dizOla(){
+    console.log("Olá")
+}
+function olaPessoa(nome){
+    console.log("Olá, " + nome)
+}
+
+// invocação
+dizOla()
+olaPessoa("Luana")
+olaPessoa("Mano")
---
+++ funcoes/func-02.js
@@ -0,0 +1,11 @@
+// Crie 4 funções para operações (+, -, *, /)
+// Calcula e imprime resultado
+
+function subtracao(n1, n2){
+    const resultado = n1 - n2
+    console.log("Resultado da subtração: " + resultado)
+}
+
+// invocaçao
+subtracao(5, 2)
+subtracao(-50, 10)
